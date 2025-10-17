2be6b467be0b807adbacbb2bb8b584be87561163
Manvi Gupta <88962337+manvigupta505@users.noreply.github.com>
2025-10-17 14:17:16 +0530

    Update LoginPage.js

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -33,7 +33,7 @@ function LoginPage() {
             method: 'POST',
             //Step 1 - Task 8
           headers: {
-            'content-type': 'application/json',
+          
             'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
           },
         //Step 1 - Task 9
