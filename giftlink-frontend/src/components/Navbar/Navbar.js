9340d2ae2c4f0f05dc2ce0c8c4358a82c778d4da
Manvi Gupta <88962337+manvigupta505@users.noreply.github.com>
2025-10-29 18:01:16 +0530

    Update Navbar.js

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -1,39 +1,17 @@
-import React, { useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import {urlConfig} from '../../config';
-import { useApp } from '../../context';
-
+import React from 'react';
 export default function Navbar() {
-    const { setIsLoggedIn, userName } = useAppContext();
-
- 
-    const profileSection=()=>{
-      navigate(`/app/profile`);
-    }
     return (
-        <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light" id='navbar_container'>
-        <a className="navbar-brand" href={`${urlConfig.backendUrl}/app`}>GiftLink</a>
-
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="/home.html">Home</a> {/* Link to home.html */}
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/app">Gifts</Link> {/* Updated Link */}
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/app/search">Search</Link>
-            </li>
-     
-          </ul>
-        </div>
-      </nav>
-        </>
-    )
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="/">GiftLink</a>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                    <a className="nav-link" href="/home.html">Home</a> {/* Link to home.html */}
+                    </li>
+                
+            
+                </ul>
+            </div>
+        </nav>
+    );
 }
